e639baa9691dc06b6d00337948b7871c6ea57776
huzekang <1040080742@qq.com>
2022-01-10 11:38:59 +0800

    调整目录

---
+++ standalone/spark3/build_standalone_image.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ${DIR}
+echo "build image in dir "${DIR}
+
+echo "start build Hadoop docker image"
+docker build -f Dockerfile_spark3 -t spark3-all-in-one .
+docker build -f Dockerfile_spark3_hive1 -t spark3-hive1-all-in-one .
