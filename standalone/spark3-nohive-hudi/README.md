63ddf2066f01bb0bff5fe02fe1508c5ba53eeb9c
huzekang <1040080742@qq.com>
2022-01-21 12:39:05 +0800

    增加hudi支持

---
+++ standalone/spark3-nohive-hudi/README.md
@@ -0,0 +1,59 @@
+# spark3集成hudi镜像
+## 前提
+docker要设置内存大于8G。
+
+## 介绍
+使用Standalone Hadoop可以在本地快速搭建hadoop环境，方便多版本hadoop调试开发。
+`Dockerfile_spark3`内置服务版本：
+- Jdk 1.8
+- Hadoop 2.7.3
+- Spark 3.1.2
+- MySQL 5.1.73
+- Hudi 0.10.0
+## 构建镜像
+当前目录下，执行`build_standalone_image.sh`脚本
+
+## 启动容器
+一旦镜像构建完成，可以启动容器。启动容器后，会自动启动服务：
+- NameNode, DataNode
+- ResourceManager, NodeManager
+- SparkThriftServer
+
+```shell script
+docker run -d \
+-m 8G \
+-p 4040:4040 \
+-p 10000:10000 \
+-p 13306:3306 \
+-p 7070:7070 \
+-p 8088:8088 \
+-p 50070:50070 \
+-p 8032:8032 \
+-p 8042:8042 \
+-p 2181:2181 \
+--name spark3-all-in-one \
+spark3-all-in-one:latest
+```
+MYSQL默认账号密码：root/123456。
+
+启动容器后，可以打开下面页面在浏览器中：
+- HDFS NameNode Web UI: [http://127.0.0.1:50070](http://127.0.0.1:50070/)
+- YARN ResourceManager Web UI: [http://127.0.0.1:8088](http://127.0.0.1:8088/)
+
+## 进入容器内调试
+```shell script
+> docker exec -it spark3-all-in-one bash
+[root@e53232ca8dc3 admin]# ls
+apache-hive-2.3.9-bin  entrypoint_hadoop.sh  jdk1.8.0_141      mysql80-community-release-el7-3.noarch.rpm  zookeeper-3.4.6
+apache-maven-3.6.1     hadoop-2.7.3          kafka_2.11-1.1.1  spark-3.1.2-bin-hadoop2.7
+
+[root@e53232ca8dc3 admin]# jps
+289 ResourceManager
+145 NameNode
+450 RunJar
+200 DataNode
+345 NodeManager
+410 JobHistoryServer
+637 Jps
+
+```
---
+++ standalone/spark3-nohive-hudi/script/spark-hudi.sql
@@ -0,0 +1,112 @@
+-- create a cow table, with default primaryKey 'uuid' and without preCombineField provided
+create table hudi_cow_nonpcf_tbl (
+  uuid int,
+  name string,
+  price double
+) using hudi;
+
+
+-- create a mor non-partitioned table without preCombineField provided
+create table hudi_mor_tbl (
+  id int,
+  name string,
+  price double,
+  ts bigint
+) using hudi
+tblproperties (
+  type = 'cow',
+  primaryKey = 'id',
+  preCombineField = 'ts'
+);
+
+-- create a partitioned, preCombineField-provided cow table
+create table hudi_cow_pt_tbl (
+  id bigint,
+  name string,
+  ts bigint,
+  dt string,
+  hh string
+) using hudi
+tblproperties (
+  type = 'cow',
+  primaryKey = 'id',
+  preCombineField = 'ts'
+ )
+partitioned by (dt, hh)
+location '/tmp/hudi/hudi_cow_pt_tbl';
+
+
+-- CTAS: create a non-partitioned cow table without preCombineField
+create table hudi_ctas_cow_nonpcf_tbl
+using hudi
+tblproperties (primaryKey = 'id')
+as
+select 1 as id, 'a1' as name, 10 as price;
+
+-- CTAS: create a partitioned, preCombineField-provided cow table
+create table hudi_ctas_cow_pt_tbl
+using hudi
+tblproperties (type = 'cow', primaryKey = 'id', preCombineField = 'ts')
+partitioned by (dt)
+as
+select 1 as id, 'a1' as name, 10 as price, 1000 as ts, '2021-12-01' as dt;
+
+
+
+insert into hudi_cow_nonpcf_tbl select 1, 'a1', 20;
+insert into hudi_mor_tbl select 1, 'a1', 20, 1000;
+-- insert dynamic partition
+insert into hudi_cow_pt_tbl partition (dt, hh)
+select 1 as id, 'a1' as name, 1000 as ts, '2021-12-09' as dt, '10' as hh;
+
+-- insert static partition
+insert into hudi_cow_pt_tbl partition(dt = '2021-12-09', hh='11') select 2, 'a2', 1000;
+
+
+
+update hudi_mor_tbl set price = price * 2, ts = 1111 where id = 1;
+update hudi_cow_pt_tbl set name = 'a1_1', ts = 1001 where id = 1;
+
+
+
+create table merge_source (id int, name string, price double, ts bigint) using hudi
+tblproperties (primaryKey = 'id', preCombineField = 'ts');
+insert into merge_source values (1, "old_a1", 22.22, 900), (2, "new_a2", 33.33, 2000), (3, "new_a3", 44.44, 2000);
+
+merge into hudi_mor_tbl as target
+using merge_source as source
+on target.id = source.id
+when matched then update set *
+when not matched then insert *
+;
+
+-- source table using parquet for testing merging into partitioned table
+create table merge_source2 (id int, name string, flag string, dt string, hh string) using parquet;
+insert into merge_source2 values (1, "new_a1", 'update', '2021-12-09', '10'), (2, "new_a2", 'delete', '2021-12-09', '11'), (3, "new_a3", 'insert', '2021-12-09', '12');
+
+merge into hudi_cow_pt_tbl as target
+using (
+  select id, name, '1000' as ts, flag, dt, hh from merge_source2
+) source
+on target.id = source.id
+when matched and flag != 'delete' then
+ update set id = source.id, name = source.name, ts = source.ts, dt = source.dt, hh = source.hh
+when matched and flag = 'delete' then delete
+when not matched then
+ insert (id, name, ts, dt, hh) values(source.id, source.name, source.ts, source.dt, source.hh)
+;
+
+
+delete from hudi_mor_tbl where id % 2 = 0;
+
+
+
+-- insert overwrite non-partitioned table
+insert overwrite hudi_mor_tbl select 99, 'a99', 20.0, 900;
+insert overwrite hudi_cow_nonpcf_tbl select 99, 'a99', 20.0;
+
+-- insert overwrite partitioned table with dynamic partition
+insert overwrite table hudi_cow_pt_tbl select 10, 'a10', 1100, '2021-12-09', '10';
+
+-- insert overwrite partitioned table with static partition
+insert overwrite hudi_cow_pt_tbl partition(dt = '2021-12-09', hh='12') select 13, 'a13', 1100;
