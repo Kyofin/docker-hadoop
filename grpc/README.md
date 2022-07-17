2e7afd8b89dd2ca2115c77acd640c346cb9b4189
huzekang <1040080742@qq.com>
2022-07-17 19:40:34 +0800

    增加protoc3支持

---
+++ grpc/README.md
@@ -0,0 +1,9 @@
+# protoc构建环境
+
+## 介绍
+可以将protoc文件挂载到容器内生成对应的java、go代码。
+
+## 使用示例
+build_go_proto.sh
+
+![](img.png)
---
+++ grpc/push_image_remote.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker tag   proto:3.21.2  peterpoker/proto:3.21.2
+
+docker push  peterpoker/proto:3.21.2
