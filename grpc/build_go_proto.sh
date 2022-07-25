c0d6caeb8df7e2ae43d71380a7fd18877bf29902
huzekang <1040080742@qq.com>
2022-07-25 21:38:47 +0800

    增加protoc的go语言grpc代码生成

--- grpc/build_go_proto.sh
+++ grpc/build_go_proto.sh
@@ -1,5 +1,5 @@
 docker build -f Dockerfile -t proto:3.21.2 .
 
-docker run -v "D:\workspace\docker-hadoop\grpc\:/data" --name grpc --rm -it   proto:3.21.2   protoc   -I=/data --go_out=/data /data/hello.proto
+docker run -v "D:\workspace\docker-hadoop\grpc\:/data" --name grpc --rm -it   proto:3.21.2   protoc   -I=/data --go_out=/data   --go-grpc_out=/data /data/hello.proto
 docker run -v "D:\workspace\docker-hadoop\grpc\:/data" --name grpc --rm -it   proto:3.21.2   protoc   -I=/data --java_out=/data  /data/hello.proto
 
