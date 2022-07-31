904f288ec2a1ed550e6830b32dab51b42bff98bd
huzekang <1040080742@qq.com>
2022-07-31 13:20:19 +0800

    增加hdfs和yarn非ha模式集群on k8s部署(方式2)

---
+++ cluster/hadoop-way2/hadoop/build.sh
@@ -0,0 +1,6 @@
+ docker build --rm -t vpemfh7/hadoop-base:latest base
+ docker build --rm -t vpemfh7/hadoop-datanode:latest datanode
+ docker build --rm -t vpemfh7/hadoop-historyserver:latest historyserver
+ docker build --rm -t vpemfh7/hadoop-namenode:latest namenode
+ docker build --rm -t vpemfh7/hadoop-nodemanager:latest nodemanager
+ docker build --rm -t vpemfh7/hadoop-resourcemanager:latest resourcemanager
