eff01c92d29dc09c8209962caab27e798505bda3
huzekang <1040080742@qq.com>
2022-08-03 14:11:16 +0800

    增加hadoop-spark-client pod，方便测试hadoop 、yarn、spark命令

---
+++ cluster/hadoop-way3-helm-chart/hadoop-spark-client/test-spark.sh
@@ -0,0 +1,2 @@
+/opt/spark-3.1.2-bin-hadoop2.7/bin/spark-submit --master yarn --deploy-mode client --class org.apache.spark.examples.SparkPi examples/jars/spark-examples_2.12-3.1.2.jar
+/opt/spark-3.1.2-bin-hadoop2.7/bin/spark-submit  --master yarn --deploy-mode cluster  --class org.apache.spark.examples.SparkPi examples/jars/spark-examples_2.12-3.1.2.jar
