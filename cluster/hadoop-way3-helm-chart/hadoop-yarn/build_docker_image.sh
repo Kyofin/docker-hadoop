f7d3f5ad707ee67670422d09605bd512a318e445
huzekang <1040080742@qq.com>
2022-09-06 18:45:04 +0800

    yarn增加dockerfile

---
+++ cluster/hadoop-way3-helm-chart/hadoop-yarn/build_docker_image.sh
@@ -0,0 +1,3 @@
+docker build -t danisla/hadoop:2.7.2 .
+docker tag danisla/hadoop:2.7.2  registry.mufankong.top/bigdata/danisla/hadoop:2.7.2
+docker push registry.mufankong.top/bigdata/danisla/hadoop:2.7.2
