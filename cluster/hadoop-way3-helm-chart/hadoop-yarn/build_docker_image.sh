83a31c24557e224316d3d61501d6415024c6a420
huzekang <1040080742@qq.com>
2022-09-07 16:47:44 +0800

    ds增加自动建库。yarn、spark、hive调整基础的hadoop镜像。调整minio默认为单机模式。mysql调优配置。

--- cluster/hadoop-way3-helm-chart/hadoop-yarn/build_docker_image.sh
+++ cluster/hadoop-way3-helm-chart/hadoop-yarn/build_docker_image.sh
@@ -1,3 +1,3 @@
-docker build -t danisla/hadoop:2.7.2 .
-docker tag danisla/hadoop:2.7.2  registry.mufankong.top/bigdata/danisla/hadoop:2.7.2
-docker push registry.mufankong.top/bigdata/danisla/hadoop:2.7.2
+docker build -t poker/hadoop:2.7.2 .
+docker tag poker/hadoop:2.7.2  registry.mufankong.top/bigdata/poker/hadoop:2.7.2
+docker push registry.mufankong.top/bigdata/poker/hadoop:2.7.2
