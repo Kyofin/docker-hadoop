65384290773c0d9c7167a6a5a1b36843ef829b22
huzekang <1040080742@qq.com>
2022-09-15 15:19:12 +0800

    增加ozone

---
+++ cluster/hadoop-way3-helm-chart/ozone/README.md
@@ -0,0 +1,6 @@
+## 安装
+```shell
+kubectl apply -f . -n bigdata-local
+kubectl delete -f . -n bigdata-local
+
+```
---
+++ cluster/hadoop-way3-helm-chart/ozone/build_docker_image.sh
@@ -0,0 +1,3 @@
+docker pull apache/ozone:1.2.0
+docker tag apache/ozone:1.2.0  registry.mufankong.top/bigdata/apache/ozone:1.2.0
+docker push registry.mufankong.top/bigdata/apache/ozone:1.2.0
