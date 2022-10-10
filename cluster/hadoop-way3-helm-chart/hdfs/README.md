bbefaccd2f7d5dd422184515458548d5155ed8e5
huzekang <1040080742@qq.com>
2022-10-10 19:00:03 +0800

    【hdfs】增加hdfs编排

---
+++ cluster/hadoop-way3-helm-chart/hdfs/README.md
@@ -0,0 +1,5 @@
+# 安装卸载
+```yaml
+helm install hdfs . -n bigdata-dev
+helm uninstall hdfs -n bigdata-dev
+```
---
+++ cluster/hadoop-way3-helm-chart/hdfs/build_hdfs_image.sh
@@ -0,0 +1,3 @@
+docker pull ccr.ccs.tencentyun.com/k8s-big-data/hadoop:v2.9.0-all
+docker tag ccr.ccs.tencentyun.com/k8s-big-data/hadoop:v2.9.0-all  registry.mufankong.top/bigdata/hadoop:v2.9.0-all
+docker push registry.mufankong.top/bigdata/hadoop:v2.9.0-all
