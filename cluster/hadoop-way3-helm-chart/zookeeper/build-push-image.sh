484a4fe370c32eb80029137f22a48e730b318874
冯延庆 <fengyq51@chinaunicom.cn>
2022-10-17 09:05:58 +0800

    feat:增加zookeeper-exporter和hive jmx

--- cluster/hadoop-way3-helm-chart/zookeeper/build-push-image.sh
+++ cluster/hadoop-way3-helm-chart/zookeeper/build-push-image.sh
@@ -8,3 +8,7 @@ echo "build image in dir "${DIR}
 docker pull zookeeper:3.5.5
 docker tag zookeeper:3.5.5  registry.mufankong.top/bigdata/zookeeper:3.5.5
 docker push registry.mufankong.top/bigdata/zookeeper:3.5.5
+
+docker pull josdotso/zookeeper-exporter:v1.1.2
+docker tag josdotso/zookeeper-exporter:v1.1.2 registry.mufankong.top/bigdata/zookeeper-exporter:v1.1.2
+docker push registry.mufankong.top/bigdata/zookeeper-exporter:v1.1.2
